a7205be78b0867a259a609b281e25ea13214a43e
Ваня <7328652@gmail.com>
2024-05-13 16:45:43 +0300

    Added Routing Code splitting Lazy Suspense

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
