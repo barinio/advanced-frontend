83ae6dba1b0473411a84c8b20dd3ad3d90f95ec2
Ванька <7328652@gmail.com>
2024-07-06 23:45:41 +0300

    15 i18n Internationalization. Define plugin. Plugin for translations

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -14,3 +14,5 @@ declare module "*.svg" {
   const svg: string;
   export default svg;
 }
+
+declare const __IS_DEV__: boolean;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import * as cls from "./Sidebar.module.scss";
 import { useState } from "react";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { LangSwitcher } from "widgets/LangSwitcher";
 
 interface SidebarProps {
   className?: string;
@@ -20,7 +21,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* <langSwitcher /> */}
+				<LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
