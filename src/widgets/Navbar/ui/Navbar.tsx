e785a7ac04bb2bbf4ed3630bfa840956d12efe4c
Ванька <7328652@gmail.com>
2024-07-06 13:33:30 +0300

    14 Sidebar. Layout of the application

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,7 +3,6 @@ import { classNames } from "shared/lib/classNames/classNames";
 import * as cls from "./Navbar.module.scss";
 
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -13,7 +12,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={cls.listLinks}>
         <AppLink theme={AppLinkTheme.INVERTED} to="./" className={cls.mainLink}>
           Go to Main
